85c28922b2281492e7f4b46e8f87ac431a8e8d00
Austin Donovan <AustinLDonovan@gmail.com>
2022-04-04 15:16:17 -0400

    question is revealed upon pressing start, timer works but is in wrong spot

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,13 @@
 
 const startButton = document.querySelector("#start-button"); // targets start button
 
-const questionContainerElement = document.querySelector("#question-container"); // targets question container
+const questionContainerElement = document.getElementById("question-container"); // targets question container
 
-var questionElement = document.querySelector("#question")
+var answerElementButtons = document.querySelector("#answer-btns") // targets answer buttons in html
 
-var answerElementButtons = document.querySelector("#answer-btns")
+var timer = document.querySelector(".time") // targets class of time in the html
 
-var timer = document.querySelector(".time")
-
-var startTime = 3
+var currentQuestionIndex = 0 // sets question index to 0
 
 
 startButton.addEventListener("click", startGame); // event listener for start game button
@@ -20,26 +18,36 @@ function startGame() { //starts game
   startTimer()
   console.log("Game has started");
   startButton.classList.add("hide"); // adds the "hide" class to the "start-btn" ID to hide the element
-  questionContainerElement.classList.remove("hide"); // removes the "hide" class to the "question-continer" ID to show the element // logs that the game has started in the console
+  questionContainerElement.removeAttribute("class"); // removes the "hide" class to the "question-continer" ID to show the element // logs that the game has started in the console
   setNextQuestion()
-};
+}
 
 function setNextQuestion() {
   showQuestion()
 }
 
 function showQuestion() {
-  questionElement.textContent = questions.question1
+  var currentQuestion = questions[currentQuestionIndex]
+  var questionTitle = document.getElementById("question")
+  questionTitle.textContent = currentQuestion.question1
+  // loop over question choices
+  // hide old question choices
+  // append buttons for each answer
+  // add event listener click for choice
+  // display selected choice on page
+
+
 }
 
 function startTimer() {
+  var startTime = 3
   setInterval(function() {
   startTime--;
-  if (startTime < 3) {
+  if (startTime >= 0) {
     timer.textContent = startTime;
   } if (startTime === 0) {
     timer.textContent = "Game Over";
-    clearInterval(startTimer);
+    clearInterval(startTime);
   }
   }, 1000);
 }
@@ -62,5 +70,14 @@ const questions = [
       {text: "C", correct: false},
       {text: "D", correct: false},
     ]
-  }
+  },
+  {
+    question1: "Do you like Dogs?",
+    answers: [
+      {text: "A", correct: true},
+      {text: "B", correct: false},
+      {text: "C", correct: false},
+      {text: "D", correct: false},
+    ]
+  },
 ]
